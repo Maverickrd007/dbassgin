a5c9430ab055bae5acf7e696622b61d85fa302d0
Maverickrd007 <raghav.duggal.s69@kalvium.community>
2025-01-31 13:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { resolve } = require('path');
-
+require('dotenv').config();
 const app = express();
 const port = 3010;
 
@@ -9,7 +9,7 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
-
+(require('mongoose')).connect(process.env.MONGO_URI).then(()=> console.log('Connected to database.')).catch((err)=>console.log('Error connecting to database: ', err));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
